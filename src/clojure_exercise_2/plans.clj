10bcae0a7c4845b555b592f79509472c3d1e3b83
Leonardo A. de Souza <leo26gpl@gmail.com>
2020-08-18 11:59:56 -0300

    Create a namespace plans.clj to def plans

---
+++ src/clojure_exercise_2/plans.clj
@@ -0,0 +1,25 @@
+(ns clojure-exercise-2.plans)
+
+(def plan1 {:supplier "eon"
+            :plan "variable"
+            :rates [{:price 13.5 :threshold 100}
+                    {:price 10}]
+            :standing_charge 0})
+
+(def plan2 {:supplier "ovo"
+            :plan "standard"
+            :rates [{:price 12.5 :threshold 300}
+                    {:price 11}]
+            :standing_charge 0})
+
+(def plan3 {:supplier "edf"
+            :plan "fixed"
+            :rates [{:price 14.5 :threshold 250}
+                    {:price 10.1 :threshold 200}
+                    {:price 9}]
+            :standing_charge 0})
+
+(def plan4 {:supplier "bg"
+            :plan "standing-charge"
+            :rates [{:price 9}]
+            :standing_charge 7})

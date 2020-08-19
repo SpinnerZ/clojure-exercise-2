b20f5b044c5ecc56274d54b57b5f14b5df144e79
Leonardo A. de Souza <leo26gpl@gmail.com>
2020-08-19 16:42:32 -0300

    Added functionality to calculate price with consumption and plan thresholds. TO-DO: Tests

--- src/clojure_exercise_2/price_calculator.clj
+++ src/clojure_exercise_2/price_calculator.clj
@@ -1,7 +1,31 @@
 (ns clojure-exercise-2.price-calculator
   (:require [clojure-exercise-2.plans :refer :all]))
 
-(def vat 1.045)
+(def vat 0.05M)
+
+(defn consumption-price
+  "Returns the charge value with the price and consumption given"
+  [price consumption]
+  (* price consumption))
+
+(defn threshold-consumption-price
+  "Returns the price calculating the consumption for each threshold given (as a map)"
+  [plan-thresholds consumption]
+  (loop [head (first plan-thresholds)
+         tail (rest plan-thresholds)
+         kwh consumption
+         charge 0]
+    ;;If kwh is 0 or less, finish and return the charge
+    (cond (<= kwh 0) charge
+
+          ;;If kwh is lesser than the threshold or if there is no threshold, finish adding the price and return the charge
+          (or (empty? tail) (< kwh (:threshold head))) (+ charge (consumption-price (:price head) kwh))
+
+          ;;If kwh is bigger than the threshold, calculate the price and prepare for the next loop
+          :else (recur (first tail)
+                       (rest tail)
+                       (- kwh (:threshold head))
+                       (+ charge (consumption-price (:price head) kwh))))))
 
 (defn add-vat
   "Returns the price rounded with VAT added to it"
@@ -9,6 +33,6 @@
   (+ price (* price vat)))
 
 (defn annual-price
-  "Calculates the annual price to all plans based on annual consumption."
+  "Given a plan and consumption, calculates the price using all threshold"
   [plan consumption]
   ())
--- test/clojure_exercise_2/price_calculator_test.clj
+++ test/clojure_exercise_2/price_calculator_test.clj
@@ -2,13 +2,13 @@
   (:require [midje.sweet :refer :all]
             [clojure-exercise-2.price-calculator :refer :all]))
 
-(facts "Testing each function"
-       (fact "Testing 5% VAT prices"
-             (add-vat 100) => 105
-             (add-vat 28.7) => 30.14
-             (add-vat 56.6) => 59.43
-             (add-vat 45.58) => 47.86
-             (add-vat 36.47) => 38.3
-             (add-vat 29.38) => 30.85
-             (add-vat 14.13) => 14.84
-             (add-vat 85.83) => 90.13))
+(facts "Testing add-vat"
+       (fact "Testing expected values"
+             (add-vat 100M) => 105M
+             (add-vat 28.7M) => 30.135M
+             (add-vat 56.6M) => 59.43M
+             (add-vat 45.58M) => 47.859M
+             (add-vat 36.47M) => 38.2935M
+             (add-vat 29.38M) => 30.849M
+             (add-vat 14.13M) => 14.8365M
+             (add-vat 85.83M) => 90.1215M))

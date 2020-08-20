dc0f48da3e238b7d6f9dd5d0c42839e515184dd9
Leonardo A. de Souza <leo26gpl@gmail.com>
2020-08-20 11:05:34 -0300

    Make consumption_calculator.clj
    TO-DO: Tests

--- src/clojure_exercise_2/consumption_calculator.clj
+++ src/clojure_exercise_2/consumption_calculator.clj
@@ -19,5 +19,33 @@
 
 (defn- price-threshold->consumption
   "Given a price and the thresholds of a plan, returns the consumption"
-  [thresholds price]
-  )
+  [thresholds charge]
+  (loop [head (first thresholds)
+         tail (rest thresholds)
+         consumption 0
+         spend charge]
+
+          ;;Spend zero means end
+    (cond (<= 0 spend) consumption
+
+          ;;If there is no tail, no need to check threshold anymore.
+          ;;If the spend is lesser than the threshold, means that it's the last iteration.
+          (or (empty? tail)
+              (< spend (:threshold head)))
+          (+ consumption (/ spend (:price head)))
+
+          ;;If the price is higher than the threshold, there is more work to do
+          :else (recur (first tail)
+                       (rest tail)
+                       (+ consumption (:threshold head))
+                       (- spend
+                          (* (:price head)
+                             (:threshold head)))))))
+
+(defn annual-usage
+  "Receives the plan and value spend in a month and calculates how much energy would be used annually"
+  [plan spend]
+  (->> (rem-vat spend)
+       (rem-standing-charge (:standing_charge plan 0))
+       (monthly-price->annual-price)
+       (price-threshold->consumption (:rates plan))))

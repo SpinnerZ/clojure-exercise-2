4f4d892eafbf309849ce4d995953e9dddd88cef5
Leonardo A. de Souza <leo26gpl@gmail.com>
2020-08-21 09:48:10 -0300

    Load plans from json

--- src/clojure_exercise_2/core.clj
+++ src/clojure_exercise_2/core.clj
@@ -1,5 +1,13 @@
 (ns clojure-exercise-2.core
-    (:require [clojure-exercise-2.price-calculator :refer :all]
-              [clojure-exercise-2.consumption-calculator :refer :all])
+    (:require [clojure-exercise-2.price-calculator :as price]
+              [clojure-exercise-2.consumption-calculator :as consumption]
+              [clojure.data.json :as json])
     (:gen-class))
+
+(def plans-path "resources/plans.json")
+
+(defn load-plans
+  "Reads the json input and puts in memory as a map"
+  [path]
+  (json/read-str (slurp path) :key-fn keyword))
 

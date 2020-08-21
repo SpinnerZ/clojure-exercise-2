6f55c017f04c0c714d7f0c6951baccf1189beb3a
Leonardo A. de Souza <leo26gpl@gmail.com>
2020-08-21 09:48:10 -0300

    Load plans from json

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [midje "1.9.9"]]
+                 [midje "1.9.9"]
+                 [org.clojure/data.json "1.0.0"]]
   :plugins [[lein-midje "3.2.1"]
             [lein-cloverage "1.2.0"]]
   :repl-options {:init-ns clojure-exercise-2.core}

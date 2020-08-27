165966e511a47bb5b7814080949ea43877cded7a
Leonardo A. de Souza <leo26gpl@gmail.com>
2020-08-26 21:06:03 -0300

    WIP -main
    Problem with initialization

--- src/clojure_exercise_2/core.clj
+++ src/clojure_exercise_2/core.clj
@@ -1,29 +1,30 @@
 (ns clojure-exercise-2.core
     (:require [clojure-exercise-2.price-calculator :as price]
               [clojure-exercise-2.consumption-calculator :as consumption]
-              [clojure.data.json :as json])
+              [clojure.data.json :as json]
+              [clojure.string :as str])
     (:gen-class))
 
-(defn load-plans
+(defn- load-plans
   "Reads the json input and puts in memory as a map"
   [path]
   (json/read-str (slurp path) :key-fn keyword))
 
 (def all-plans (load-plans "resources/plans.json"))
 
-(defn single-price-consumption
+(defn- single-price-consumption
   "Returns the supplier, plan and total cost from an annual consumption"
   [consumption plan]
   {:supplier (:supplier plan)
    :plan (:plan plan)
    :total-cost (price/annual-price plan consumption)})
 
-(defn all-plans-price-consumption
+(defn- all-plans-price-consumption
   "Gets all plans price given a consumption input, sorting them by cheapest first"
-  [consumption plans]
+  [plans consumption]
   (sort-by :total-cost (map (partial single-price-consumption consumption) plans)))
 
-(defn print-prices
+(defn- print-prices
   "Prints to console the supplier, plan and total-cost from given seq or unique map"
   [plans]
   (run! #(println
@@ -34,3 +35,27 @@
                   2
                   java.math.RoundingMode/HALF_UP)))
         plans))
+
+(defn- print-consumption
+  "Prints to console the kWh of a given plan and price"
+  [plan spend]
+  (println (consumption/annual-usage plan spend)))
+
+(def print-prices-all-plans (comp print-prices (partial all-plans-price-consumption all-plans)))
+
+(defn -main
+  "nuSwitch Energy Comparison"
+  [& args]
+  (loop [input args]
+        (let [command (first input)
+              values (rest input)]
+          (condp = command
+           "price" (do (print-prices-all-plans (int (first values)))
+                       (flush)
+                       (recur (str/split (read-line) #" ")))
+           "usage" (do (print-consumption (filter #(= (first values) (:plan %))
+                                                  all-plans)
+                                          (last values))
+                       (flush)
+                       (recur (str/split (read-line) #" ")))
+           "exit" nil))))
